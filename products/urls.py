6600dc4d932b7c7434ccb38ec38537f34cdf1d4b
octav <octavsteaua11@gmail.com>
2021-11-18 19:30:31 +0200

    CRUD for products

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     # cautam un integer in path si acelasi nume trebuie sa il aiba si variabila din antetul functie
     # daca aici este id1 si noi apelam functia index atunci un antetul functiei trebuie sa fie variabila id1
-    path('', views.index, name='index'),
+    path('show/', views.list_products, name='list_products'),
+    path('show/<str:p_name>/', views.detailed_product, name='detailed_product'),
+    path('createProduct/', views.create_product, name='create_product'),
+    path('delete/<str:p_name>/', views.delete_product, name='delete_product'),
+    path('update/<str:p_name>/', views.update_product, name='update_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,15 +1,48 @@
 import pymongo
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import Product
 from .forms import ProductForm
 
 
-def index(request):
+def list_products(request):
+    products = Product.objects.all()
+    return render(request, "products/productsList.html", {"products": products})
+
+
+def detailed_product(request, p_name):
+    product = Product.objects.get(product_name=p_name)
+    return render(request, "products/productsDetails.html", {"product": product})
+
+
+def create_product(request):
     form = ProductForm()
     if request.method == "POST":
         form = ProductForm(request.POST)
         if form.is_valid():
             form.save()
-    return render(request, "products/productsFrom.html", {"form": form})
+            return redirect("list_products")
+    return render(request, "products/productsForm.html", {"form": form})
+
+
+def delete_product(request, p_name):
+    product = Product.objects.get(product_name=p_name)
+    if request.method == "POST":
+        yes = request.POST.get("Yes")
+        if yes is not None:
+            product.delete()
+            return redirect('list_products')
+    return render(request, 'products/productsDelete.html', {'product': product})
+
+def update_product(request, p_name):
+    product = Product.objects.get(product_name=p_name)
+    form = ProductForm(instance=product)
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('list_products')
+
+    return render(request, 'products/productsUpdate.html', {'form': form})
